ba3511551dbe751252c5dd51420e5226c6136bb6
Sathish Kannan R <sathishkannan.ramalingam@gmail.com>
2025-03-12 14:50:48 +0100

    Update pydecorator.py
    2nd decorator example

--- pydecorator.py
+++ pydecorator.py
@@ -24,3 +24,21 @@ def result(mark):
 
 results =  result([45,70,80,32,66,90])
 print("Results:", results)
+
+
+
+### Example 2 ####
+# This is our "sticker" (decorator)
+def say_hi(func):
+    def new_toy(n):
+        print("Hi!")
+        func(n)  # The original toy still works
+    return new_toy
+
+# This is our toy (function)
+@say_hi  # Slap the sticker on!
+def toy(n):
+    print(f"I’m a robot toy! van {n}")
+
+# Play with the toy
+toy("sathish")
